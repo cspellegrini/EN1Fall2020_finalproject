a4edc01df7da430579e8f8bb2e1111381d2fb4c3
christopher pellegrini <christopher.pellegrini@tufts.edu>
2020-12-11 03:00:31 -0500

    added secret key

--- src/secret.js
+++ src/secret.js
@@ -16,7 +16,7 @@ var getAPIusername = ""; // fill in with your name (if left blank, assuming usin
 // (2) Hardcode your API key information here
 
 // AIRTABLE SUPPORT
-var airtable_api = "key" + "XXXXXXXXXXX";     // fill in your info
+var airtable_api = "key" + "gDQPykvhSXN53d";     // fill in your info
 var airtable_baseid = "app" + "YYYYYYYYYYYY"; // fill in your info
 
 // SYSTEMLINK SUPPORT
